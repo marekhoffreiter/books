6aa6c9e0e0b19fc9a845496b743a65dbb8263bcc
Marek Hoffreiter <marekhoffreiter@gmail.com>
2018-10-01 23:09:41 +0200

    On branch master

--- src/Book/Book.js
+++ src/Book/Book.js
@@ -6,7 +6,7 @@ const book = (props) => {
   return (
     <li key={`${props.title}${props.author}`} className='Book'>
       <img src={`${baseUrl}${props.imageLink}`} className='Book img'/> 
-             Author: {props.author} | Title: {props.title} | Year: {props.year}
+        Author: {props.author} | Title: {props.title} | Year: {props.year}
     </li>
   )
 };
