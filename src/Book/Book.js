890fd92cd969138bad2fbbe7dd203d2ee65877b7
Marek Hoffreiter <marekhoffreiter@gmail.com>
2018-10-01 23:08:48 +0200

    styling

--- src/Book/Book.js
+++ src/Book/Book.js
@@ -1,14 +1,13 @@
 import React from 'react';
-import baseUrl from './BaseUrl'
+import baseUrl from './BaseUrl';
+import './Book.css';
 
 const book = (props) => {
-    
-    return (
-        <li key={`${props.title}${props.author}`}>
-            <img src={`${baseUrl}${props.imageLink}`}/>
-            Author: {props.author} | Title: {props.title} | Year: {props.year}
-            ></li>
-    )
+  return (
+    <li key={`${props.title}${props.author}`} className='Book'>
+      <img src={`${baseUrl}${props.imageLink}`} className='Book img'/> 
+             Author: {props.author} | Title: {props.title} | Year: {props.year}
+    </li>
+  )
 };
-
 export default book;
